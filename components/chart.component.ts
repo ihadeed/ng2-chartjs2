1e8743d31334ccfd5397b05d622482e975908176
Ibrahim Hadeed <ibby93@gmail.com>
2016-06-01 07:00:30 -0400

    init

---
+++ components/chart.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'chart',
+    template: `
+    <canvas width="400" height="400"></canvas>
+    `
+})
+export class ChartComponent {
+
+
+
+}
