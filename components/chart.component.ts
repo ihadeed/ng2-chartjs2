da42a2b824370d26e9a6f27db94d9e7bc4420a7d
Ibrahim Hadeed <ibby93@gmail.com>
2016-06-01 07:19:43 -0400

    add some stuff

--- components/chart.component.ts
+++ components/chart.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, Input, Output, EventEmitter} from '@angular/core';
+
+declare var Chart: any;
 
 @Component({
     selector: 'chart',
@@ -8,6 +10,47 @@ import {Component} from '@angular/core';
 })
 export class ChartComponent {
 
+    private chart: any;
+    private canvas: HTMLCanvasElement;
+    private ctx: CanvasRenderingContext2D;
 
+    @Input() private options: ChartOptions;
 
+    constructor (element: ElementRef) {
+        this.canvas = element.nativeElement.children[0];
+        this.ctx = this.canvas.getContext("2d");
+        this.chart = new Chart(this.ctx, this.options);
+    }
+
+}
+
+export const enum ChartType {
+    'bar',
+    'line',
+    'radar',
+    'polarArea',
+    'doughnut'
+}
+
+export interface ChartOptions {
+    type: ChartType;
+    data: {
+        labels: Array<string>;
+        datasets: Array<{
+            label: string;
+            data: Array<number>;
+            backgroundColor?: Array<string>;
+            borderColor?: Array<string>;
+            borderWidth?: number;
+        }>;
+    };
+    options?: {
+        scales?: {
+            yAxes?: Array<{ticks?: {beginAtZero: boolean}}>
+        },
+        responsive?: boolean,
+        hover?: {
+            mode?: string;
+        }
+    };
 }
