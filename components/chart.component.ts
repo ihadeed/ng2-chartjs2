2027b940f832963478b46512f26874362b8a768e
Ibby Hadeed <ibby93@gmail.com>
2016-07-22 13:58:57 -0400

    working

--- components/chart.component.ts
+++ components/chart.component.ts
@@ -1,56 +1,66 @@
-import {Component, ElementRef, Input, Output, EventEmitter} from '@angular/core';
+import {Component, ElementRef, Input, Output, EventEmitter, OnInit, OnDestroy, OnChanges} from '@angular/core';
 
 declare var Chart: any;
 
 @Component({
-    selector: 'chart',
-    template: `
-    <canvas width="400" height="400"></canvas>
-    `
+  selector: 'chart',
+  template: `
+    <canvas></canvas>
+    `,
+  styles: [':host {display: block;}']
 })
-export class ChartComponent {
+export class ChartComponent implements OnInit, OnDestroy, OnChanges {
 
-    private chart: any;
-    private canvas: HTMLCanvasElement;
-    private ctx: CanvasRenderingContext2D;
+  private chart: any;
+  private canvas: HTMLCanvasElement;
+  private ctx: CanvasRenderingContext2D;
 
-    @Input() private options: ChartOptions;
 
-    constructor (element: ElementRef) {
-        this.canvas = element.nativeElement.children[0];
-        this.ctx = this.canvas.getContext("2d");
-        this.chart = new Chart(this.ctx, this.options);
-    }
+  @Input() private options: ChartOptions;
+
+  constructor (private element: ElementRef) {}
+
+  ngOnInit(): void {
+    console.log(this.element);
+    this.canvas = this.element.nativeElement.children[0];
+    this.ctx = this.canvas.getContext("2d");
+    this.chart = new Chart(this.ctx, this.options);
+    console.log(this.chart);
+    window['c'] = this.chart;
+  }
+
+  ngOnDestroy(): void {
+
+  }
+
+  ngOnChanges(): void {
+
+  }
 
 }
-
-export const enum ChartType {
-    'bar',
-    'line',
-    'radar',
-    'polarArea',
-    'doughnut'
-}
+export type ChartType = 'line' | 'bar' | 'radar' | 'polarArea' | 'doughnut' | 'pie';
 
 export interface ChartOptions {
-    type: ChartType;
-    data: {
-        labels: Array<string>;
-        datasets: Array<{
-            label: string;
-            data: Array<number>;
-            backgroundColor?: Array<string>;
-            borderColor?: Array<string>;
-            borderWidth?: number;
-        }>;
-    };
-    options?: {
-        scales?: {
-            yAxes?: Array<{ticks?: {beginAtZero: boolean}}>
-        },
-        responsive?: boolean,
-        hover?: {
-            mode?: string;
-        }
-    };
+  type: ChartType;
+  data: {
+    labels: string[];
+    datasets: ChartDataset[];
+  };
+  options?: {
+    scales?: {
+      yAxes?: Array<{ticks?: {beginAtZero: boolean}}>
+    },
+    responsive?: boolean,
+    hover?: {
+      mode?: string;
+    }
+  };
+}
+
+export interface ChartDataset {
+  label: string;
+  data: number[];
+  backgroundColor?: string[];
+  borderColor?: string[];
+  borderWidth?: number;
 }
